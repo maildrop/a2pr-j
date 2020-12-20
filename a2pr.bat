a1ba3981dc9d05b12512a74e172a966168698ad5
TOGURO Mikito <maildrop@users.noreply.github.com>
2020-12-20 17:57:48 +0900

    jar

--- a2pr.bat
+++ a2pr.bat
@@ -1,2 +1,7 @@
 @echo off
-java -jar "%~dp0\a2pr-1.0-SNAPSHOT.jar" %1 %2 %3 %4 %5 %6 %7 %8 %9 
+setlocal
+if exist "%~dp0\a2pr-1.0-SNAPSHOT.jar" set jarfile="%~dp0\a2pr-1.0-SNAPSHOT.jar"
+if exist "%~dp0\a2pr-1.0-SNAPSHOT-jar-with-dependencies.jar" set jarfile="%~dp0\a2pr-1.0-SNAPSHOT-jar-with-dependencies.jar"
+
+if DEFINED jarfile (java -jar "%~dp0\a2pr-1.0-SNAPSHOT-jar-with-dependencies.jar" %1 %2 %3 %4 %5 %6 %7 %8 %9) else (echo file not found "a2pr-1.0-SNAPSHOT.jar" or "a2pr-1.0-SNAPSHOT-jar-with-dependencies.jar" )
+endlocal
